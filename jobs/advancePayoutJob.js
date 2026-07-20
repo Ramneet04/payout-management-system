5ae9d03ce5245dfa5163a5c4fae6f2923c3744fb
Ramneet Singh <ramneetsinghrtd@gmail.com>
2026-07-21 03:31:25 +0530

    fixed a typo error

--- jobs/advancePayoutJob.js
+++ jobs/advancePayoutJob.js
@@ -19,7 +19,7 @@ async function runAdvancePayoutJob(){
             await Transaction.create([
                 {
                     userId: sale.userId,
-                    saleid: sale._id,
+                    saleId: sale._id,
                     status: 'success',
                     amount: advanceAmount,
                     type: "advance"
